48034eafd4edf4b1d1194cd179628228bbbe98cf
Gleyser Guimaraes <gleyser.bonfim.guimaraes@ccc.ufcg.edu.br>
2014-08-09 10:15:37 -0300

    tirando numeros magicos

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,6 +5,7 @@ import java.util.Calendar;
 import java.util.List;
 import java.util.Random;
 
+import models.GuardaNumero;
 import models.Evento;
 import models.Participante;
 import models.Tema;
@@ -44,52 +45,52 @@ public class Application extends Controller {
 			
 			temas.add(Tema.DESAFIOS);
 			temas.add(Tema.PROGRAMACAO);
-			auxiliar("Python na mente e coração", "Neste evento iremos debater e propor soluções para novas releases.", 7, temas, eventos);
+			auxiliar("Python na mente e coração", "Neste evento iremos debater e propor soluções para novas releases.", new GuardaNumero(7).getNumero(), temas, eventos);
 									
 			temas = new ArrayList<Tema>();
 			temas.add(Tema.ARDUINO);
 			temas.add(Tema.ELETRONICA);
-			auxiliar("Luta de robôs", "Traga seu robô feito em arduino e traga para competir com outros.", 3, temas, eventos);
+			auxiliar("Luta de robôs", "Traga seu robô feito em arduino e traga para competir com outros." ,new GuardaNumero(3).getNumero() , temas, eventos);
 			
 			temas = new ArrayList<Tema>();
 			temas.add(Tema.DESAFIOS);
 			temas.add(Tema.PROGRAMACAO);
-			auxiliar("IV Olímpiadas de programação da UFCG", "Traga sua equipe e venha competir nessa maratona de programação.", 1, temas, eventos);
+			auxiliar("IV Olímpiadas de programação da UFCG", "Traga sua equipe e venha competir nessa maratona de programação.", new GuardaNumero(1).getNumero(), temas, eventos);
 			
 			temas = new ArrayList<Tema>();
 			temas.add(Tema.DESAFIOS);
 			temas.add(Tema.PROGRAMACAO);
-			auxiliar("II Encontro para programadores de Python", "O encontro contará com a participação de um de seus fundadores, inúmeras palestras e maratonas. Não percam!!", 12, temas, eventos);
+			auxiliar("II Encontro para programadores de Python", "O encontro contará com a participação de um de seus fundadores, inúmeras palestras e maratonas. Não percam!!", new GuardaNumero(12).getNumero() , temas, eventos);
 			
 	
 			temas = new ArrayList<Tema>();
 			temas.add(Tema.PROGRAMACAO);
 			temas.add(Tema.DESAFIOS);
-			auxiliar("III Semana da Computação Verde", "Exiba sua proposta para uma computação mais verde e concorra a diversos prêmios", 3, temas, eventos);
+			auxiliar("III Semana da Computação Verde", "Exiba sua proposta para uma computação mais verde e concorra a diversos prêmios", new GuardaNumero(3).getNumero(), temas, eventos);
 			
 			temas = new ArrayList<Tema>();
 			temas.add(Tema.PROGRAMACAO);
 			temas.add(Tema.WEB);
-			auxiliar("Web em foco", "Este evento contará com a participação de um dos fundadores da Web, e juntos iremos compartilhar diversas dicas e boas práticas nessa vasta área.",17, temas, eventos);
+			auxiliar("Web em foco", "Este evento contará com a participação de um dos fundadores da Web, e juntos iremos compartilhar diversas dicas e boas práticas nessa vasta área.",new GuardaNumero(17).getNumero(), temas, eventos);
 			
 			temas = new ArrayList<Tema>();
 			temas.add(Tema.ELETRONICA);
 			temas.add(Tema.ARDUINO);
-			auxiliar("Minicurso Arduino", "Evento destinado a alunos de LOAC, caso sobre vagas iremos disponibilizar em breve", 5, temas, eventos);
+			auxiliar("Minicurso Arduino", "Evento destinado a alunos de LOAC, caso sobre vagas iremos disponibilizar em breve", new GuardaNumero(5).getNumero(), temas, eventos);
 				
 			temas = new ArrayList<Tema>();
 			temas.add(Tema.ELETRONICA);
 			temas.add(Tema.ARDUINO);
-			auxiliar("Curto circuito", "Evento sobre circuitos eletrônicos, venha dar curto em seus circuitos também!!", 21, temas, eventos);
+			auxiliar("Curto circuito", "Evento sobre circuitos eletrônicos, venha dar curto em seus circuitos também!!", new GuardaNumero(21).getNumero(), temas, eventos);
 				
 			temas = new ArrayList<Tema>();
 			temas.add(Tema.DESAFIOS);
-			auxiliar("VI Encontro de Docentes de CC", "Evento para debatermos propostas e soluções para os problemas enfrentados pelos alunos de CC.", 15, temas, eventos);
+			auxiliar("VI Encontro de Docentes de CC", "Evento para debatermos propostas e soluções para os problemas enfrentados pelos alunos de CC.", new GuardaNumero(15).getNumero(), temas, eventos);
 			
 			temas = new ArrayList<Tema>();
 			temas.add(Tema.PROGRAMACAO);
 			temas.add(Tema.DESAFIOS);
-			auxiliar("Café com Java", "Curso destinado apenas a alunos cursando a disciplina LP2.", 8, temas, eventos);
+			auxiliar("Café com Java", "Curso destinado apenas a alunos cursando a disciplina LP2.", new GuardaNumero(8).getNumero(), temas, eventos);
 						
 			return eventos;
 		} catch (EventoInvalidoException e) {
@@ -110,27 +111,27 @@ public class Application extends Controller {
 	private static void criarParticipacoesFake(List<Evento> eventos) {
 		Random rnd = new Random();
 		try {
-			int numDeRandon = 3;
-			criarParticipacao(new Participante("Alberto Leça", "alberto_leca@mail.com", eventos.get(rnd.nextInt(numDeRandon))));
-			criarParticipacao(new Participante("Alberto Leça", "alberto_leca@mail.com", eventos.get(rnd.nextInt(numDeRandon))));
-			criarParticipacao(new Participante("Alberto Leça", "alberto_leca@mail.com", eventos.get(rnd.nextInt(numDeRandon))));
-			criarParticipacao(new Participante("Belmifer Linares", "belmifer_linares@mail.com", eventos.get(rnd.nextInt(numDeRandon))));
-			criarParticipacao(new Participante("Belmifer Linares", "belmifer_linares@mail.com", eventos.get(rnd.nextInt(numDeRandon))));
-			criarParticipacao(new Participante("Célia Rúa", "celia_rua@mail.com", eventos.get(rnd.nextInt(numDeRandon))));
-			criarParticipacao(new Participante("Deolindo Castello Branco", "deolindo_castello@mail.com", eventos.get(rnd.nextInt(numDeRandon))));
-			criarParticipacao(new Participante("Doroteia Pasos", "doroteia_passos@mail.com", eventos.get(rnd.nextInt(numDeRandon))));
-			criarParticipacao(new Participante("Eugénio Palhares", "eugenio_palhares@mail.com", eventos.get(rnd.nextInt(numDeRandon))));
-			criarParticipacao(new Participante("Fausto Furtado", "fausto_furtado@mail.com", eventos.get(rnd.nextInt(numDeRandon))));
-			criarParticipacao(new Participante("Filipa Leiria", "filipa_leiria@mail.com", eventos.get(rnd.nextInt(numDeRandon))));
-			criarParticipacao(new Participante("Leonilde Figueiredo", "leonilde_figueiredo@mail.com", eventos.get(rnd.nextInt(numDeRandon))));
-			criarParticipacao(new Participante("Pascoal Caldeira", "pascoal_caldeira@mail.com", eventos.get(rnd.nextInt(numDeRandon))));
-			criarParticipacao(new Participante("Paula Lousado", "paula_lousado@mail.com", eventos.get(rnd.nextInt(numDeRandon))));
-			criarParticipacao(new Participante("Quitério Galindo","quiterio_galindo@mail.com", eventos.get(rnd.nextInt(numDeRandon))));
-			criarParticipacao(new Participante("Rosa Varejão", "rosa_varejao@mail.com", eventos.get(rnd.nextInt(numDeRandon))));
-			criarParticipacao(new Participante("Sonia Gabeira", "sonia_gabeira@mail.com", eventos.get(rnd.nextInt(numDeRandon))));
-			criarParticipacao(new Participante("Érico Albuquerque", "erico_albuquerque@mail.com", eventos.get(rnd.nextInt(numDeRandon))));
-			criarParticipacao(new Participante("Érico Albuquerque", "erico_albuquerque@mail.com", eventos.get(rnd.nextInt(numDeRandon))));
-			criarParticipacao(new Participante("Tairine Reis", "tairine_reis@mail.com", eventos.get(rnd.nextInt(numDeRandon))));
+			GuardaNumero numDeRandon = new GuardaNumero(3);
+			criarParticipacao(new Participante("Alberto Leça", "alberto_leca@mail.com", eventos.get(rnd.nextInt(numDeRandon.getNumero()))));
+			criarParticipacao(new Participante("Alberto Leça", "alberto_leca@mail.com", eventos.get(rnd.nextInt(numDeRandon.getNumero()))));
+			criarParticipacao(new Participante("Alberto Leça", "alberto_leca@mail.com", eventos.get(rnd.nextInt(numDeRandon.getNumero()))));
+			criarParticipacao(new Participante("Belmifer Linares", "belmifer_linares@mail.com", eventos.get(rnd.nextInt(numDeRandon.getNumero()))));
+			criarParticipacao(new Participante("Belmifer Linares", "belmifer_linares@mail.com", eventos.get(rnd.nextInt(numDeRandon.getNumero()))));
+			criarParticipacao(new Participante("Célia Rúa", "celia_rua@mail.com", eventos.get(rnd.nextInt(numDeRandon.getNumero()))));
+			criarParticipacao(new Participante("Deolindo Castello Branco", "deolindo_castello@mail.com", eventos.get(rnd.nextInt(numDeRandon.getNumero()))));
+			criarParticipacao(new Participante("Doroteia Pasos", "doroteia_passos@mail.com", eventos.get(rnd.nextInt(numDeRandon.getNumero()))));
+			criarParticipacao(new Participante("Eugénio Palhares", "eugenio_palhares@mail.com", eventos.get(rnd.nextInt(numDeRandon.getNumero()))));
+			criarParticipacao(new Participante("Fausto Furtado", "fausto_furtado@mail.com", eventos.get(rnd.nextInt(numDeRandon.getNumero()))));
+			criarParticipacao(new Participante("Filipa Leiria", "filipa_leiria@mail.com", eventos.get(rnd.nextInt(numDeRandon.getNumero()))));
+			criarParticipacao(new Participante("Leonilde Figueiredo", "leonilde_figueiredo@mail.com", eventos.get(rnd.nextInt(numDeRandon.getNumero()))));
+			criarParticipacao(new Participante("Pascoal Caldeira", "pascoal_caldeira@mail.com", eventos.get(rnd.nextInt(numDeRandon.getNumero()))));
+			criarParticipacao(new Participante("Paula Lousado", "paula_lousado@mail.com", eventos.get(rnd.nextInt(numDeRandon.getNumero()))));
+			criarParticipacao(new Participante("Quitério Galindo","quiterio_galindo@mail.com", eventos.get(rnd.nextInt(numDeRandon.getNumero()))));
+			criarParticipacao(new Participante("Rosa Varejão", "rosa_varejao@mail.com", eventos.get(rnd.nextInt(numDeRandon.getNumero()))));
+			criarParticipacao(new Participante("Sonia Gabeira", "sonia_gabeira@mail.com", eventos.get(rnd.nextInt(numDeRandon.getNumero()))));
+			criarParticipacao(new Participante("Érico Albuquerque", "erico_albuquerque@mail.com", eventos.get(rnd.nextInt(numDeRandon.getNumero()))));
+			criarParticipacao(new Participante("Érico Albuquerque", "erico_albuquerque@mail.com", eventos.get(rnd.nextInt(numDeRandon.getNumero()))));
+			criarParticipacao(new Participante("Tairine Reis", "tairine_reis@mail.com", eventos.get(rnd.nextInt(numDeRandon.getNumero()))));
 		} catch (PessoaInvalidaException e) { }
 	}
 	
