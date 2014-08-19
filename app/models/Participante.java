e3f3d3c0de2f398dafad0c869b6d0f3f53608905
Gleyser Guimaraes <gleyser.bonfim.guimaraes@ccc.ufcg.edu.br>
2014-08-19 13:33:35 -0300

    melhorando o sonar

--- app/models/Participante.java
+++ app/models/Participante.java
@@ -39,9 +39,9 @@ public class Participante {
 
 	public Participante(String nome, String email, Evento evento)
 			throws PessoaInvalidaException {
-		setNome(nome);
-		setEmail(email);
-		setEvento(evento);
+		this.nome = nome;
+		this.email =email;
+		this.evento = evento;
 	}
 
 	public String getNome() {
@@ -49,10 +49,14 @@ public class Participante {
 	}
 
 	public void setNome(String nome) throws PessoaInvalidaException {
-		if (nome == null)
+		if (nome == null){
 			throw new PessoaInvalidaException("Parametro nulo");
-		if (nome.length() > 70)
+		}
+			
+		if (nome.length() > 70){
 			throw new PessoaInvalidaException("Nome longo");
+		}
+			
 		this.nome = nome;
 	}
 
@@ -61,12 +65,18 @@ public class Participante {
 	}
 
 	public void setEmail(String email) throws PessoaInvalidaException {
-		if (email == null)
+		if (email == null){
 			throw new PessoaInvalidaException("Parametro nulo");
-		if (!email.matches(EMAIL_PATTERN))
+		}
+			
+		if (!email.matches(EMAIL_PATTERN)){
 			throw new PessoaInvalidaException("Email inválido");
-		if (email.length() > 70)
+		}
+			
+		if (email.length() > 70){
 			throw new PessoaInvalidaException("Email longo");
+		}
+			
 		this.email = email;
 	}
 
@@ -75,8 +85,10 @@ public class Participante {
 	}
 
 	public void setEvento(Evento evento) throws PessoaInvalidaException {
-		if (evento == null)
+		if (evento == null){
 			throw new PessoaInvalidaException("Parametro nulo");
+		}
+			
 		this.evento = evento;
 	}
 }
